e34b003b364f79a1924395b7676b7b5b0529f630
ahbanna <alhasanulbanna@gmail.com>
2023-12-17 09:23:29 +0600

    organize schema

--- src/app/modules/user.model.ts
+++ src/app/modules/user.model.ts
@@ -1,34 +1,46 @@
 import { Schema, model } from 'mongoose';
-import { User } from './user/user.interface';
+import { Address, FullName, Order, User } from './user/user.interface';
 
+const fullNameSchema = new Schema<FullName>({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+});
+const addressSchema = new Schema<Address>({
+  street: { type: String, required: true },
+  city: { type: String, required: true },
+  country: { type: String, required: true },
+});
+const orderSchema = new Schema<Order>({
+  productName: { type: String, required: true },
+  price: { type: Number, required: true },
+  quantity: { type: Number, required: true },
+});
 const userSchema = new Schema<User>({
   userId: { type: Number, required: true, unique: true },
   username: { type: String, required: true, unique: true },
   password: { type: String },
   fullName: {
-    firstName: {
-      type: String,
-      required: true,
-    },
-    lastName: {
-      type: String,
-      required: true,
-    },
+    type: fullNameSchema,
+    required: true,
   },
   age: { type: Number, required: true },
   email: { type: String, required: true },
   isActive: { type: Boolean, required: true },
   hobbies: [{ type: String, required: true }],
   address: {
-    street: { type: String, required: true },
-    city: { type: String, required: true },
-    country: { type: String, required: true },
+    type: addressSchema,
+    required: true,
   },
   orders: [
     {
-      productName: { type: String, required: true },
-      price: { type: Number, required: true },
-      quantity: { type: Number, required: true },
+      type: orderSchema,
+      required: true,
     },
   ],
 });
